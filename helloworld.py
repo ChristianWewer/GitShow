d304faa50e3f4e9c7eb68e3c2dadae4c25726952
CJC\chw <chw@cjc.dk>
2019-11-26 10:49:26 +0100

    helloworld.py - Fixed input prompt text - Added get_user_input to program flow

--- helloworld.py
+++ helloworld.py
@@ -1,6 +1,11 @@
 print("hello world!")
 
+print("What is your name?")
+name = get_user_input()
+print("Hello {}".format(name))
+
+
 
 def get_user_input():
 	""" This functions gets user input and retuns it as a string """
-	return input("Pleas")
+	return input(">")
